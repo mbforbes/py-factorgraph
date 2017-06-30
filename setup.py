47db9200800715c65f00b23f063d70faeb61434e
Maxwell Forbes <m.b.forbes@gmail.com>
2017-06-30 12:03:24 -0700

    (Hopefully) fix bug in setup.py to allow installation to work.

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setup(
     packages=['factorgraph'],
     url='https://github.com/mbforbes/py-factorgraph/',
     license='MIT',
-    description='Factor graph and loopy belief propagation.',
-    long_description=open('README.md').read(),
+    description='Factor graphs and loopy belief propagation library.',
     install_requires=[
         "numpy >= 1.13.0",
     ],
